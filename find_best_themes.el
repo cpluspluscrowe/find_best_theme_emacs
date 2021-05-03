5bf116e6994c6332fd46469472fac2c714625a49
ccrowe <ccrowe@unomaha.edu>
2021-05-03 07:16:47 -0700

    cycle until you find the best theme

--- find_best_themes.el
+++ find_best_themes.el
@@ -1,23 +1,27 @@
 
-(setq themes (custom-available-themes))
-
-(defun try-themes (theme) 
+(defun try-theme (theme) 
   "function to try each theme"
-  (let ((theme-name (symbol-name theme))) 
-    (progn (disable-theme (car custom-enabled-themes)) 
-           (load-theme theme t) 
-           (message "%s" theme) 
-           (if (y-or-n-p (symbol-name theme)) 
-               (progn theme-name (print theme-name)) nil ; return theme name if I like it
-               ))))
+  (if (eq (type-of theme) 'string) 
+      (try-themes (intern-soft theme)) 
+    (progn (let ((theme-name (symbol-name theme))) 
+             (disable-theme (car custom-enabled-themes)) 
+             (load-theme theme t) 
+             (message "%s" theme) 
+             (if (y-or-n-p (symbol-name theme)) 
+                 (progn theme-name (print theme-name)) nil ; return theme name if I like it
+                 )))))
 
-(setq good-theme-names (-map #'try-themes themes))
+(defun find-best-theme (themes) 
+  (progn
+                                        ; remove all existing themes
+    (while (disable-theme (car custom-enabled-themes))) 
+    (cond ((eq (length themes) 1) 
+           (car themes)) 
+          (t (find-best-theme (remove nil (-map #'try-theme themes)))))))
 
-; Themes I like
-;"doom-monokai-classic"
-;"doom-old-hope"
-;"doom-outrun-electric"
-;"deeper-blue"
-;"misterioso"
-;"tango-dark"
-;"tsdh-dark"
+                                        ; work through themes recursively until one is left
+(setq best-theme (find-best-theme (custom-available-themes)))
+(try-theme best-theme)
+
+                                        ; now repeat until the length is one.
+                                        ; "misterioso"
